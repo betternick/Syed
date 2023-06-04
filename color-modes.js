8a6a8dca553251b9b5b49ef6d135f161bdb4df91
syedwaqarahmed <syedw@student.ubc.ca>
2023-06-03 20:38:06 -0700

    alignment fixed

--- color-modes.js
+++ color-modes.js
@@ -3,6 +3,11 @@
  * Copyright 2011-2023 The Bootstrap Authors
  * Licensed under the Creative Commons Attribution 3.0 Unported License.
  */
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2023 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
 
 (() => {
   "use strict";
